5d377cf4e4fa81e27b1b3d50dc846af6aa990ce2
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2026-01-22 12:51:15 +0200

    commit

--- first_project_flutter/lib/home_screen.dart
+++ first_project_flutter/lib/home_screen.dart
@@ -1,18 +1,69 @@
+import 'package:first_project_flutter/counter.dart';
+import 'package:first_project_flutter/signup_page.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+  final List _pages = [HomeScreen(), SignupPage(), Counter()];
+  void navegateIndex(int index) {
+    pageIndex = index;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu),
         title: Text('First App'),
         centerTitle: true,
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text('MENU')),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/homepage");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.login),
+              title: Text("Login"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/loginpage");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.login_rounded),
+              title: Text("Sign Up"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/singuppage");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.countertops),
+              title: Text("Counter"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/counter');
+              },
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
@@ -49,6 +100,13 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Counter'),
         ],
       ),
     );
--- first_project_flutter/lib/main.dart
+++ first_project_flutter/lib/main.dart
@@ -1,7 +1,10 @@
 // import 'package:first_project_flutter/home_screen.dart';
 import 'package:first_project_flutter/counter.dart';
+import 'package:first_project_flutter/forget_password_page.dart';
+import 'package:first_project_flutter/home_screen.dart';
 import 'package:first_project_flutter/login_page.dart';
 import 'package:first_project_flutter/messenger_screen.dart';
+import 'package:first_project_flutter/signup_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +16,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Counter());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      routes: {
+        '/homepage': (context) => HomeScreen(),
+        '/loginpage': (context) => LoginPage(),
+        '/singuppage': (context) => SignupPage(),
+        '/messenger': (context) => MessengerScreen(),
+        '/forgetpassword': (context) => ForgetPasswordPage(),
+        '/counter': (context) => Counter(),
+      },
+    );
   }
 }
